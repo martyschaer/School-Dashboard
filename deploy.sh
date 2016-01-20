a80d9d38f12cc0eaf3e9ca2ce055ce2c5a6151f5
Severin Kaderli <severin.kaderli@gmail.com>
2016-01-20 20:53:25 +0100

    This is getting a little annoying...

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Change to the html directory and clone the repository
 cd /var/www/html
-git clone https://github.com/martyschaer/SchoolProject --branch $TRAVIS_BRANCH .
+git clone https://github.com/martyschaer/SchoolProject --branch $1 .
 composer install
 cp /var/www/.env /var/www/html
