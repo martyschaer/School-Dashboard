3c4acf3816837dc3e98e9cbc691895548c02df43
Severin Kaderli <severin.kaderli@gmail.com>
2016-01-20 20:40:17 +0100

    Added current branch to git clone command

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+#Change to the html directory and clone the repository
 cd /var/www/html
-git clone https://github.com/martyschaer/SchoolProject .
+git clone https://github.com/martyschaer/SchoolProject --branch $TRAVIS_BRANCH .
 composer install
 cp /var/www/.env /var/www/html
