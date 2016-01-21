3687b4798eddc8dbe9ffacbe95390a785384b043
Severin Kaderli <severin.kaderli@gmail.com>
2016-01-21 13:25:15 +0100

    Made logging clearer

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+echo "Starting deploy script: $(date +%Y-%m-%d.%H:%M:%S)"
 cd /var/www/html
-echo "Branch: $1"
+echo "Checking out branch: $1"
 git clone https://github.com/martyschaer/SchoolProject --branch $1 .
+echo "Finished git cloning; Starting composer"
 composer install
+echo "Composer finished; Copying .env file"
 cp /var/www/.env /var/www/html
