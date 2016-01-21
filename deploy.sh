cbe8842951cedefe8a6761af932c30a2da4ac689
Severin Kaderli <severin.kaderli@gmail.com>
2016-01-21 13:15:59 +0100

    Optimized the logging

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-# clone the newest version
 cd /var/www/html
-logDir=/var/www/log
-echo "$PWD" >> $logDir/deploy.log
-echo "Branch: $1" >> $logDir/deploy.log
-git clone https://github.com/martyschaer/SchoolProject --branch $1 . >> $logDir/deploy.log
-composer install >> $logDir/deploy.log
-cp /var/www/.env /var/www/html >> $logDir/deploy.log
+echo "Branch: $1"
+git clone https://github.com/martyschaer/SchoolProject --branch $1 .
+composer install
+cp /var/www/.env /var/www/html
