ae2e238b17b15e23c33fa87037bb449aad92d3a0
Severin Kaderli <severin.kaderli@gmail.com>
2016-01-21 12:58:53 +0100

    Added some logging output

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 # clone the newest version
 cd /var/www/html
+echo "$PWD" >> log.stuff
+echo "Branch: $1" >> log.stuff
 git clone https://github.com/martyschaer/SchoolProject --branch $1 .
 composer install
 cp /var/www/.env /var/www/html
