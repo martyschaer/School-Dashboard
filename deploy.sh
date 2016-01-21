527acd65df7558487c7e9d449a56863f5f5aac90
Severin Kaderli <severin.kaderli@gmail.com>
2016-01-21 13:11:22 +0100

    Added more logging to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 
 # clone the newest version
 cd /var/www/html
-echo "$PWD" >> log.stuff
-echo "Branch: $1" >> log.stuff
-git clone https://github.com/martyschaer/SchoolProject --branch $1 .
-composer install
-cp /var/www/.env /var/www/html
+logDir=/var/www/log
+echo "$PWD" >> $logDir/deploy.log
+echo "Branch: $1" >> $logDir/deploy.log
+git clone https://github.com/martyschaer/SchoolProject --branch $1 . >> $logDir/deploy.log
+composer install >> $logDir/deploy.log
+cp /var/www/.env /var/www/html >> $logDir/deploy.log
