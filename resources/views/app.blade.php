2cc4687f29aefc9d53e6c8e5d479a6a7ea46880f
Severin Kaderli <severin.kaderli@gmail.com>
2016-02-02 15:53:41 +0100

    Added js and css links

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,50 +1,29 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
+        <title>School-Dashboard</title>
+
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">
-        <title>Laravel</title>
+        <link rel="stylesheet" type="text/css" href="{{URL::asset('../bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{URL::asset('../bower_components/bootstrap-material-design/dist/css/bootstrap-material-design.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{URL::asset('../bower_components/bootstrap-material-design/dist/css/ripples.min.css')}}">
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <script src="{{URL::asset('../bower_components/jquery/dist/jquery.min.js')}}"></script>
+        <script src="{{URL::asset('../bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+        <script src="{{URL::asset('../bower_components/bootstrap-material-design/dist/js/material.min.js')}}"></script>
+        <script src="{{URL::asset('../bower_components/bootstrap-material-design/dist/js/ripples.min.js')}}"></script>
+        
+        
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title small {
-                font-size: 48px;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title"><strong>The Project</strong><br>Kaderli <br>&<br> Sch&auml;r<br><small>Coming soon</small></div>
-            </div>
-        </div>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </body>
 </html>
