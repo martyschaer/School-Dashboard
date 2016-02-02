ceef859a41f02398a05834f1227702490ce8e785
Severin Kaderli <severin.kaderli@gmail.com>
2016-02-02 18:34:46 +0100

    Added bower configuration file.
    The bower components are now located in the public/vendor folder.
    Reinstalling of the bower components is needed.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,25 +5,28 @@
         <title>School-Dashboard</title>
 
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">
-        <link rel="stylesheet" type="text/css" href="{{URL::asset('../bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{URL::asset('../bower_components/bootstrap-material-design/dist/css/bootstrap-material-design.min.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{URL::asset('../bower_components/bootstrap-material-design/dist/css/ripples.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{URL::asset('vendor/bootstrap/dist/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{URL::asset('vendor/bootstrap-material-design/dist/css/bootstrap-material-design.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{URL::asset('vendor/bootstrap-material-design/dist/css/ripples.min.css')}}">
 
-        <script src="{{URL::asset('../bower_components/jquery/dist/jquery.min.js')}}"></script>
-        <script src="{{URL::asset('../bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
-        <script src="{{URL::asset('../bower_components/bootstrap-material-design/dist/js/material.min.js')}}"></script>
-        <script src="{{URL::asset('../bower_components/bootstrap-material-design/dist/js/ripples.min.js')}}"></script>
+        <script src="{{URL::asset('vendor/jquery/dist/jquery.min.js')}}"></script>
+        <script src="{{URL::asset('vendor/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+        <script src="{{URL::asset('vendor/bootstrap-material-design/dist/js/material.min.js')}}"></script>
+        <script src="{{URL::asset('vendor/bootstrap-material-design/dist/js/ripples.min.js')}}"></script>
         
         
 
     </head>
     <body>
-    <h1>Hello World!</h1>
+    <main class="container">
+        <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </main>
+    
     </body>
 </html>
