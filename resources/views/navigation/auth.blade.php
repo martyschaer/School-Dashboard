2f128e469eb0214741c1c67097535f4ae7fd0e24
Severin Kaderli <severin.kaderli@gmail.com>
2016-02-08 19:43:44 +0100

    Rearranged the views and made seperate partials for the navigation and footer.

---
+++ resources/views/navigation/auth.blade.php
@@ -0,0 +1,9 @@
+<ul class="nav navbar-nav">
+  <li class="active"><a href="#">Home</a></li>
+</ul>
+welcome you are logged in ...
+<form class="navbar-form navbar-right">
+  <div class="form-group">
+    <input type="text" class="form-control col-md-8" placeholder="Search">
+  </div>
+</form>
---
+++ resources/views/navigation/guest.blade.php
@@ -0,0 +1,10 @@
+<ul class="nav navbar-nav">
+  <li class="active"><a href="#">Home</a></li>
+  <li><a href="#">Login</a></li>
+  <li><a href="#">Register</a></li>
+</ul>
+<form class="navbar-form navbar-right">
+  <div class="form-group">
+    <input type="text" class="form-control col-md-8" placeholder="Search">
+  </div>
+</form>
