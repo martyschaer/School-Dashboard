a9198bb0d9c2d40055d6abeaca11f47229f13ca7
Severin Kaderli <severin.kaderli@gmail.com>
2016-02-09 08:38:40 +0100

    Added create_tasks migration.

---
+++ database/migrations/2016_02_09_073025_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('description');
+            $table->boolean('is_done');
+            $table->integer('user_id')->unsigned();
+            //TODO: Add more fields for time of done, parent_id and so on...
+            $table->timestamps();
+
+            //Foreign keys
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
