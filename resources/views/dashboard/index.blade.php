0d87bfb9e04c21acb63d85d6a0c322c268ad6c55
Severin Kaderli <severin.kaderli@gmail.com>
2016-02-09 09:42:43 +0100

    Worked on the dashboar

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,31 +1,55 @@
 @extends('app')
 @section('content')
 
-@foreach($tasks as $task)
-<div class='task col-md-12'>
-    <div class="col-md-1">
-        <input type="checkbox" name="task{{$task->id}}"
-        @if($task->is_done)
-            checked
-        @endif
-        >
-    </div>
-    <div class="col-md-4">
-        <p
-        @if($task->is_done)
-            class='done'
-        @endif
-        >
-        {{$task->description}}
-        </p>
-    </div>
-    <div class="col-md-1">
-X
-    </div>
+<div class="calendar col-md-8">
+    @for($i = 1; $i<=5;$i++)
+        <div class="cell col-md-2">
+        <br>
+        {{$i}}
+        <br>
+        </div>
+    @endfor
+    @for($i = 1; $i<=5;$i++)
+        <div class="cell col-md-2">
+        <br>
+        {{$i}}
+        <br>
+        </div>
+    @endfor
+    @for($i = 1; $i<=5;$i++)
+        <div class="cell col-md-2">
+        <br>
+        {{$i}}
+        <br>
+        </div>
+    @endfor
 
 </div>
+<div class="tasks col-md-4">
+
+    @foreach($tasks as $task)
+    <div class='task col-md-12'>
+        <div class="col-md-1">
+            <input type="checkbox" name="task{{$task->id}}"
+            @if($task->is_done)
+                checked
+            @endif
+            >
+        </div>
+        <div class="col-md-9">
+            <p
+            @if($task->is_done)
+                class='done'
+            @endif
+            >
+            {{$task->description}}
+            </p>
+        </div>
+        <div class="col-md-1"><i class="fa fa-trash"></i></div>
+
+    </div>
 
 
-@endforeach
-
+    @endforeach
+</div>
 @endsection
