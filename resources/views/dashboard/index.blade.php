a9ab620a6083474ea31cfdbc0d498295ed62c213
Severin Kaderli <severin.kaderli@gmail.com>
2016-02-09 13:54:37 +0100

    Added temporary timetable

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,31 +1,39 @@
 @extends('app')
 @section('content')
 
-<div class="calendar col-md-8">
-    @for($i = 1; $i<=5;$i++)
-        <div class="cell col-md-2">
-        <br>
-        {{$i}}
-        <br>
-        </div>
-    @endfor
-    @for($i = 1; $i<=5;$i++)
-        <div class="cell col-md-2">
-        <br>
-        {{$i}}
-        <br>
-        </div>
-    @endfor
-    @for($i = 1; $i<=5;$i++)
-        <div class="cell col-md-2">
-        <br>
-        {{$i}}
-        <br>
-        </div>
-    @endfor
+<!-- Timetable -->
+<!-- TODO: This is just temporary to have something to show -->
+<div id="timetable" class="col-md-8">
+    <table class="table table-bordered table-hover">
+        <thead>
+            <tr>
+                <th>Time</th>
+                <th>Montag</th>
+                <th>Dienstag</th>
+                <th>Mittwoch</th>
+                <th>Donnerstag</th>
+                <th>Freitag</th>
+            </tr>
+        </thead>
+        <tbody>
+            @for($i=1;$i <=48;$i++)
+                <tr>
+                @for($j=0;$j < 6;$j++)
+                    @if($j==0)
+                    <td>{{$i/2}}</td>
+                    @else
+                    <td>&nbsp;</td>
+                    @endif
+                @endfor
+                </tr>
+            @endfor
+        </tbody>
+    </table>
 
 </div>
-<div class="tasks col-md-4">
+
+<!-- Tasks -->
+<div id="tasks" class="col-md-4">
 
     @foreach($tasks as $task)
     <div class='task col-md-12'>
@@ -48,8 +56,27 @@
         <div class="col-md-1"><i class="fa fa-trash"></i></div>
 
     </div>
-
-
     @endforeach
+</div>
+
+<!-- Agenda -->
+<!-- TODO: Only temporary like the timetable -->
+<br>
+<div id="agenda" class="col-md-12">
+    <table class="table table-bordered table-hover">
+        <thead>
+            <tr>
+                <th>
+                Date:
+                </th>
+                <th>
+                Task:
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
 </div>
 @endsection
