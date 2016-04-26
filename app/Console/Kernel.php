cefc574a822c6cb1aba6497c7c856e739dc9144f
Severin Kaderli <severin.kaderli@gmail.com>
2016-04-26 18:36:51 +0200

    [#31] Sending reminder 30 minutes before due date

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,18 +4,13 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Task;
+use Mail;
+use App\User;
+use Carbon\Carbon;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        Commands\Inspire::class,
-    ];
-
     /**
      * Define the application's command schedule.
      *
@@ -24,7 +19,20 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
+        $schedule->call(function() {
+            $tasks = Task::all()->where("is_done", 0)
+                                ->where("reminder_sent", 0)
+                                ->where("due_at", ">=", Carbon::now()->subMinutes(30));
+
+            foreach($tasks as $task) {
+                $user = $task->user;
+                $task->reminder_sent = 1;
+                $task->update();
+                Mail::send('email.reminder', ["user" => $user, "task" => $task], function($m) use($user) {
+                    $m->from('hello@app.com', 'School-Dashboard');
+                    $m->to($user->email, $user->email)->subject("Reminder for task!");
+                });
+            }
+        });
     }
 }
---
+++ resources/views/email/reminder.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Reminder</title>
+</head>
+<body>
+    <h1>Hello {{$user->email}}</h1>
+    <p>
+        We are reminding you of your task: "{{$task->description}}". The task is due at "{{$task->due_at->format("Y-m-d H:i:s")}}"
+    </p>
+    <p>- School-Dashboard</p>
+</body>
+</html>
